6cc1fea986f30265c89665931ca7ea129082abc4
saifulislam142348 <saiful17182103353@gmail.com>
2024-06-12 11:35:30 +0600

    aded

---
+++ src/data/data.js
@@ -0,0 +1,34 @@
+const  value={
+    name:"Saiful islam",
+    date:"10-10-2-24",
+}
+const  address="Tangail";
+const imageInfo={
+    image:"https://images.app.goo.gl/8UHxRnLc8MdcYTJ38",
+    title:"image view",
+}
+
+const people = [
+    {
+        image:'https://images.generated.photos/zRuly38LuHo-upVQfxZjUb6YXOHXapy58gXXFglv1Rg/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MDAzMzE4LmpwZw.jpg',
+        name: 'Jane Doe',
+        designation: 'CEO, Doe Corporation',
+        location: '968 Ocala Street, Orlando, Florida'
+    },
+    {
+        image:'https://images.generated.photos/dVrq4AXS8SmlQ_HTyxG4bJRmi4IulUA4omUbvvSyQMU/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NDkwODEzLmpwZw.jpg',
+        name: 'John Doe',
+        designation: 'CTO, Doe Corporation',
+        location: '2970 Duke Lane, Brunswick, New Jersey'
+    },
+    {
+        image:'https://images.generated.photos/XkTVulI9UnbMVl1veOfrra6bS2v0QIqLbSIQrgXni14/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MTYxODM1LmpwZw.jpg',
+        name: 'Jimmy Doe',
+        designation: 'CMO, Doe Corporation',
+        location: '3679 Elk Street, Denver, Colorado'
+    }
+]
+
+
+export default value;
+export  {address,imageInfo,people};
